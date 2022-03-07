ce256d94c8d3e0b6a0e8ed42759fc08c4f582555
losgif <losgif@gmail.com>
2022-03-07 14:19:46 +0800

    build: v1.4.3

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import chalk from 'chalk'
 import * as fs from 'fs'
+import ora from 'ora'
 import { Arguments } from 'yargs'
 
 type Options = {
-  config?: string | undefined
+  host?: string | undefined
 }
 
 const module = {
@@ -13,15 +14,21 @@ const module = {
   builder: (yargs) =>
     yargs
       .options({
-        config: {
-          alias: 'c',
+        host: {
+          alias: 'H',
           describe: '编译配置文件',
           type: 'string'
         }
       }),
   handler: async (argv: Arguments<Options>): Promise<void> => {
-    axios.get('https://osstest.tf56.com/teamWorkApi/v2/api-docs')
+    const url = argv.host ?? 'https://osstest.tf56.com'
+
+    const spinner = ora(`正在从 ${ url } 处加载API文件`).start()
+
+    axios.get(`${ url }/teamWorkApi/v2/api-docs`)
       .then(({ data: swagger }) => {
+        spinner.text = '正在处理数据'
+
         for (const pathsKey in swagger.paths) {
           const path = swagger.paths[pathsKey]
 
@@ -31,9 +38,11 @@ const module = {
           }
         }
 
+        spinner.text = '数据写入中...'
         const swaggerBuffer = Buffer.from(JSON.stringify(swagger))
 
         fs.writeFile('swagger.json', swaggerBuffer, err => {
+          spinner.stop()
           if (err) {
             process.stdout.write(chalk.red('写入文件失败'))
             process.exit(1)
@@ -41,6 +50,7 @@ const module = {
         })
       })
       .catch(error => {
+        spinner.stop()
         console.log(error)
         process.stdout.write(chalk.red('接口文件获取失败'))
 
