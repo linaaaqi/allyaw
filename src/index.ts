3f071563627ba831bf6758cfaf052bb64b894c05
losgif <losgif@gmail.com>
2022-03-07 09:36:37 +0800

    build: v1.4.1

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import compile from './commands/compile.js';
+import generate from './commands/generate.js'
 
 yargs(hideBin(process.argv))
   // Use the command to scaffold.
   .command(compile)
+  .command(generate)
   // Enable strict mode.
   .strict()
   // Useful aliases.
